de20a446707eace753a5d3fd7687994c6917c8ee
Mahyar - Mortezaei <96655803+Scorpian-my@users.noreply.github.com>
2023-08-01 03:52:16 +0330

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+window.addEventListener('load', function() {
+    // Hide the loading container once the page is fully loaded
+    document.getElementById('loading-container').style.display = 'none';
+  });
+  
