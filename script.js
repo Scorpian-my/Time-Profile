df5b82ef3e96d61a687e80b84bd30301d2888784
Mahyar - Mortezaei <96655803+Scorpian-my@users.noreply.github.com>
2023-12-28 23:21:31 +0330

    Delete script.js

--- script.js
+++
@@ -1,5 +0,0 @@
-window.addEventListener('load', function() {
-    // Hide the loading container once the page is fully loaded
-    document.getElementById('loading-container').style.display = 'none';
-  });
-  
